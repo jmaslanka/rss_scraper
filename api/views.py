ce5906de6108dc33608be1c28163452ae46fe783
Jakub Maślanka <jacob.maslanka@protonmail.com>
2020-02-12 22:37:39 +0100

    Remove unused imports

--- api/views.py
+++ api/views.py
@@ -1,8 +1,4 @@
 from rest_framework import status
-from rest_framework.mixins import (
-    ListModelMixin,
-    RetrieveModelMixin,
-)
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
